5524bf8151912fea95d651f3b69eb572cecc6651
otgon <otgon.petru@gmail.com>
2024-05-07 22:22:41 +0200

    Migrated to MySQL, added Asset Entity (using Lombok)

--- src/main/java/com/Anka/asset/Asset.java
+++ src/main/java/com/Anka/asset/Asset.java
@@ -1,14 +1,25 @@
 package com.Anka.asset;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Document(collection = "assets")
-public record Asset(
-        @Id Integer id,
-        String ticker,
-        String name,
-        String sector,
-        String asset_type
-) {
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "asset")
+public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "ticker", length = 20)
+    private String ticker;
+    @Column(name = "name", length = 50)
+    private String name;
+    @Column(name = "sector", length = 50)
+    private String sector;
+    @Column(name = "asset_type", length = 50)
+    private String asset_type;
+
 }
